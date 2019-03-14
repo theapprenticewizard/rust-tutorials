8f14350b31de32c1e016fa580608f943cad376e6
theapprenticewizard <rayd3900@yahoo.com>
2019-03-13 20:23:30 -0400

    add methods

---
+++ methods/target/debug/deps/methods-302ab07c7add04ec.d
@@ -0,0 +1,5 @@
+C:\Users\Euler\rust_projects\methods\target\debug\deps\methods-302ab07c7add04ec.exe: src\main.rs
+
+C:\Users\Euler\rust_projects\methods\target\debug\deps\methods-302ab07c7add04ec.d: src\main.rs
+
+src\main.rs:
---
+++ methods/target/debug/methods.d
@@ -0,0 +1,1 @@
+C:\Users\Euler\rust_projects\methods\target\debug\methods.pdb: C:\Users\Euler\rust_projects\methods\src\main.rs
